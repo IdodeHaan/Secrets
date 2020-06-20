1fb115c7d4c0687ffd18554560256a2f98a41f96
Haan <ido.de.haan@sogeti.com>
2020-06-20 14:49:48 +0200

    working version with bcrypt hashing and salting

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+// const md5 = require("md5");
+// const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
@@ -16,7 +19,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
@@ -32,16 +35,18 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
@@ -52,11 +57,13 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === req.body.password) {
-          res.render("secrets");
-        } else {
-          console.log("user not found");
-        }
+        bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+      } else {
+        console.log("user not found");
       }
     }
   });
